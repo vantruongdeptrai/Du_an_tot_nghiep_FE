aaf04806b726a8663ac724d521f0bb31291cae98
anhbth <yodoll2003@gmail.com>
2024-09-27 19:59:52 +0700

    Add product and variant

--- admin-respo/src/api/products/types.ts
+++ admin-respo/src/api/products/types.ts
@@ -1,18 +1,19 @@
 
 
 export type Product = {
-    id?: string;
+    id: string;
     name: string;
     image: string;
     price: string;
     description: string;
-    category_id: string;
+    category_id: number;
     sale_price:string;
     sale_start:string;
     sale_end:string;
-    new_product:boolean;
-    best_seller_product:boolean;
-    featured_product:boolean;
+    new_product:number;
+    best_seller_product:number;
+    featured_product:number;
+    is_variants: number;
 }
 
 export type ProductInput = {
@@ -28,4 +29,17 @@ export type ProductInput = {
     new_product:number;
     best_seller_product:number;
     featured_product:number;
+    is_variants: number;
+}
+
+export type ProductVariant = {
+    id?: string;
+    product_id?: string;
+    image: string;
+    size_id: string;
+    color_id: string;
+    price: string;
+    quantity: string;
+    status: string;
+    sku: string;
 }
