025edaa70d0d09995cb64e14ba83c50e2441286d
anhbth <yodoll2003@gmail.com>
2024-10-21 22:00:59 +0700

    Css for sign in

--- client/src/hooks/useRegister.ts
+++ client/src/hooks/useRegister.ts
@@ -19,21 +19,6 @@ interface LoginInfo {
 // }
 
 const useAuth = () => {
-  const loginUser = async (loginInfo: LoginInfo) => {
-    try {
-      const response = await axios.post('http://127.0.0.1:8000/api/login', {
-        email: loginInfo.email,
-        phone: loginInfo.phone,
-        password: loginInfo.password
-      });
-      
-      // Trả về dữ liệu phản hồi từ server nếu đăng nhập thành công
-      return response.data;
-    } catch (error) {
-      // Xử lý lỗi nếu đăng nhập không thành công
-      throw new Error(error.response?.data?.message || 'Đăng nhập thất bại');
-    }
-  };
   
   
   const registerUser = async (data) => {
@@ -45,7 +30,7 @@ const useAuth = () => {
         console.error(error.response.data);
       }
   }
-  return {loginUser, registerUser}
+  return {registerUser}
 }
 
 
